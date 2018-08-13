69b7ca266ccc25a9c5731560f64f8e3102925d75
youp-han <youph@hotmail.com>
2018-08-13 18:01:31 +0900

    second commit

---
+++ HelloApps/Hello/Hello1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello
+{
+    class Hello1: IHello 
+    {
+
+        #region IHello Members
+
+        public string sayHello(string name)
+        {
+            return "안녕하세요 " + name + " 씨!";
+        }
+
+        #endregion
+    }
+}
---
+++ HelloApps/HelloApps/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Spring.Context;
+using Spring.Context.Support;
+using Hello;
+
+namespace HelloApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            IHello hello = (IHello)ctx.GetObject("MyHello");
+            string name = "Mike";
+            string result = hello.sayHello(name);
+            Console.WriteLine(result);
+        }
+    }
+}
